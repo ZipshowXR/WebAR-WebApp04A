d45e8aa638398a4d5d6e782fed24423c0fb99d0e
hasel20 <zipshowxr@gmail.com>
2022-07-04 13:17:09 +0900

    a

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR-WebApp04A.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ver_Modeling_000.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
